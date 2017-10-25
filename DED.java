d3a4999c30e1bd19170c320d18f2f9d9eb0f1e3b
Richard Supak <richard.supak@icloud.com>
2017-10-25 07:09:54 -0500

    Update DED.java

--- DED.java
+++ DED.java
@@ -1,12 +1,41 @@
+//Filename: DED.java
+//Rebuild of DEDivided, rebuild of DeadlyEncounters
+//Written by Richard Supak
+//Written on 24October2017
 
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.PrintWriter;
+import java.util.Random;
 
 public class DED {
   public static void main(String[] args) {
+
+    //set imported methods (Scanner, Random, PrintWriter)
+    Scanner keyboard = new Scanner(System.in);
+    PrintWriter pw = new PrintWriter(System.out, true);
+    Random roll = new Random();
+
+    //create player
     Player playerOne = new Player();
 
     playerOne.setPlayerStats();
-    playerOne.displayPlayer();
-    playerOne.setExp(50);
+
+    boolean addXP = true;
+    while(addXP == true) {
+      playerOne.displayPlayer();
+      pw.println("Enter XP gained:");
+      int exp = keyboard.nextInt();
+      playerOne.setExp(exp);
+      pw.println("Continue? (y/n)");
+      String answer = keyboard.next();
+      if (answer.equals("n")) {
+        addXP = false;
+      }
+    }
+
 
   }
 }
